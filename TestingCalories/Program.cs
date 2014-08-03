5109104f3359f25d867cb310e116659ef4fffd35
jrotmi <jeffotmi@hotmail.com>
2014-08-03 15:17:42 +0200

    WorkingCalCalcWithoutMethods
    To Check With Nino

---
+++ TestingCalories/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingCalories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many calories did you burn today?");
+            string caloriesBurned = Console.ReadLine();
+            Console.WriteLine("How many calories did you eat today?");
+            string caloriesEaten = Console.ReadLine();
+
+            int outCalories;
+            bool isValid = int.TryParse(caloriesBurned, out outCalories);
+            int inCalories;
+            bool isValidTwo = int.TryParse(caloriesEaten, out inCalories);
+            if ((isValid) && (isValidTwo))
+            {
+                int calorieDeficit;
+                int calorieSurplus;
+
+
+                if (outCalories > inCalories)
+                {
+                    calorieDeficit = outCalories - inCalories;
+                    Console.WriteLine("You reached a calorie deficit of {0}", calorieDeficit);
+                }
+                else if (outCalories < inCalories)
+                {
+                    calorieSurplus = inCalories - outCalories;
+                    Console.WriteLine("You reached a calorie surplus of {0}", calorieSurplus);
+                }
+                else if (outCalories == inCalories)
+                {
+                    Console.WriteLine("You ate exactly as many calories as you burned!");
+                }
+                else
+                {
+                    Console.WriteLine("Something went wrong! Please check your input!");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
